2380bac317a59842b21cfad936e47837b22c0f99
Vanishagupta18 <guptavanisha11@gmail.com>
2025-09-10 16:38:07 +0000

    ARRAYS

--- Arrays.js
+++ Arrays.js
@@ -14,24 +14,34 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 // myArr.unshift(11)
 // myArr.shift() 
-console.log(myArr);
+// console.log(myArr);
 
-console.log(myArr.includes(9));
-console.log(myArr.indexOf(3));
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
 
-console.log(myArr.join("-"));
+// console.log(myArr.join("-"));
 
 
 // slice, splice
 
-console.log("A ", myArr);
+// console.log("A ", myArr);
 
-const myn1 = myArr.slice(1, 3)
+// const myn1 = myArr.slice(1, 3)
 
-console.log(myn1);
-console.log("B ", myArr);
+// console.log(myn1);
+// console.log("B ", myArr);
 
 
-const myn2 = myArr.splice(1, 3)
-console.log("C ", myArr);
-console.log(myn2);
+// const myn2 = myArr.splice(1, 3)
+// console.log("C ", myArr);
+// console.log(myn2);
+
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
