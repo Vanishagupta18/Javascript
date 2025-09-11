a835fa943a4fd6ace29e16187b507900f0874c16
Vanishagupta18 <guptavanisha11@gmail.com>
2025-09-11 15:33:19 +0000

    objects update

--- objects.js
+++ objects.js
@@ -3,10 +3,12 @@
 //object literal: no singleton is created
 //Object.create is used to create a new object with the specified prototype object and properties.
 
+const mySym=Symbol("key1")
 
 const JsUser = {
     name: "Hitesh",
     "full name": "Hitesh Choudhary",
+    [mySym]: "value1",
     age: 18,
     location: "Jaipur",
     email: "hitesh@google.com",
@@ -14,6 +16,24 @@ const JsUser = {
     lastLoginDays: ["Monday", "Saturday"]
 }
 
-console.log(JsUser.email)//not better to use
-console.log(JsUser["email"])//better to use 
+console.log(JsUser.email)
+console.log(JsUser["email"])
 console.log(JsUser["full name"])
+console.log(JsUser[mySym])//correct way to access symbol property
+
+JsUser.email="vani@gmail.com"
+// Object.freeze(JsUser)//it will not allow to change the object properties
+
+JsUser.email="hello@gmail.com"//it will not change the email as object is freezed
+console.log(JsUser)
+
+JsUser.greeting=function(){
+    console.log("Hello JsUser")
+}
+
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+console.log(JsUser.greetingTwo())
+JsUser.greeting()
