446227b13f5b1901ec22fff82cbdf6215ae2cb5c
Vanishagupta18 <guptavanisha11@gmail.com>
2025-09-17 04:57:41 +0000

    scopes

--- scopes.js
+++ scopes.js
@@ -1,17 +1,18 @@
-var c = 300
-let a = 300
+var c = 300//dont use var
+let a = 300//global scope
+
 if (true) {
-    let a = 10
+    let a = 10//block scope
     const b = 20
     console.log("INNER: ", a);
-    
+    var c = 40//dont use var
 }
 
 
 
 console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
 
 function one(){
     const username = "hitesh"
@@ -22,9 +23,35 @@ function one(){
         console.log(website);
     }
     // console.log(website);
+    two()
+ }
+one()
 
-     two()
- 
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
 }
 
-one()
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+// addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
