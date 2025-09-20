d4daf4ae79d0866b6789bec91be3df63c70b1d05
Vanishagupta18 <guptavanisha11@gmail.com>
2025-09-20 16:36:30 +0000

    control statements

---
+++ control_flow.js
@@ -0,0 +1,52 @@
+// let age = 18;
+// if (age >= 18) {
+//   console.log("You are an adult.");
+// }
+
+
+let age = 16;
+if (age >= 18) {
+  console.log("You can vote.");
+} else {
+  console.log("You are too young to vote.");
+}
+
+let marks = 85;
+if (marks >= 90) {
+  console.log("Grade A");
+} else if (marks >= 75) {
+  console.log("Grade B");
+} else {
+  console.log("Grade C");
+}
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
