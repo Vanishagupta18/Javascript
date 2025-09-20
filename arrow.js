aa0233f6d66530d2f9456aa941cf95245851072e
Vanishagupta18 <guptavanisha11@gmail.com>
2025-09-20 15:35:02 +0000

    arrow functions

--- arrow.js
+++ arrow.js
@@ -33,7 +33,21 @@ const user = {
 // chai()
 
 
-const addTwo = (num1, num2) => {
-    return num1 + num2
-}
-console.log(addTwo(4, 5));
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+// console.log(addTwo(4, 5));
+
+//const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
