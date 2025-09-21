2d27450062f5adb320a481cde804cffe99505b37
Vanishagupta18 <guptavanisha11@gmail.com>
2025-09-21 15:17:19 +0000

    array iteration

--- iterations.js
+++ iterations.js
@@ -18,4 +18,9 @@
 //    }
 // }
 
-
+let myArray = ["flash", "batman", "superman"]
+console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+    }
