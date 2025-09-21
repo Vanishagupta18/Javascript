703fdffb4e769e070f0642e4194313656f908a44
Vanishagupta18 <guptavanisha11@gmail.com>
2025-09-21 15:07:38 +0000

    updates

--- control_flow.js
+++ control_flow.js
@@ -50,3 +50,8 @@ switch (month) {
         break;
 }
 
+// Ternary Operator
+
+// condition ? true : false
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
