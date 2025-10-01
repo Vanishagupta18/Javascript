81bf78bf895ebaf23aa62c4c9c9e3513b27896af
Vanishagupta18 <guptavanisha11@gmail.com>
2025-10-01 08:35:17 +0000

    random color

--- project5/script.js
+++ project5/script.js
@@ -1,20 +1,53 @@
-let intervalId; // store the interval ID
+// let intervalId; // store the interval ID
 
 
-const RandomColor = () => {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-    return `rgb(${r}, ${g}, ${b})`;
-}
-console.log(RandomColor());
-document.querySelector("#start").addEventListener("click", () => {
-   intervalId = setInterval(() => {
-        document.body.style.backgroundColor = RandomColor();
-    }, 1000);
-});
+// const RandomColor = () => {
+//     const r = Math.floor(Math.random() * 256);
+//     const g = Math.floor(Math.random() * 256);
+//     const b = Math.floor(Math.random() * 256);
+//     return `rgb(${r}, ${g}, ${b})`;
+// }
+// console.log(RandomColor());
+// document.querySelector("#start").addEventListener("click", () => {
+//    intervalId = setInterval(() => {
+//         document.body.style.backgroundColor = RandomColor();
+//     }, 1000);
+// });
 
-document.querySelector("#stop").addEventListener("click", () => {
-    clearInterval(intervalId);
-    console.log("Stopped");
-});
+// document.querySelector("#stop").addEventListener("click", () => {
+//     clearInterval(intervalId);
+
+//     console.log("Stopped");
+// });
+
+// another solution
+// generate a random color
+
+const randomColor = function () {
+  const hex = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+let intervalId;
+const startChangingColor = function () {
+  if (!intervalId) {
+    intervalId = setInterval(changeBgColor, 1000);
+  }
+
+  function changeBgColor() {
+    document.body.style.backgroundColor = randomColor();
+  }
+};
+const stopChangingColor = function () {
+  clearInterval(intervalId);
+  intervalId = null;
+};
+
+document.querySelector('#start').addEventListener('click', startChangingColor);
+
+document.querySelector('#stop').addEventListener('click', stopChangingColor);
+
